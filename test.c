6c13611cd6f99d8bf33de99868356accb2a607a5
hvhq <64365465+hvhq@users.noreply.github.com>
2020-12-11 23:33:53 +0700

    change test file

---
+++ test.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<string.h>
+#include<unistd.h>
+
+int main(){
+    int ret, fd;
+    int value;
+    
+    fd = open("/dev/ranIntDevice", O_RDONLY); //Open file device
+    if (fd < 0){
+        perror("Failed to open file\n");
+        return errno;
+    }
+
+    //Read number in mynull
+    ret = read(fd, &value, sizeof(value));
+    if (ret < 0){
+        perror("Failed to read random number\n");
+        return errno;
+    }
+
+    printf("Random number is %d\n", value);
+    
+    return 0;
+}
